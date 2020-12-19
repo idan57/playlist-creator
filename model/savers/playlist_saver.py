74ba4ae11af1a32134e9ced6ed9f2fb1b368dd7f
idan57 <47928001+idan57@users.noreply.github.com>
2020-12-19 19:51:21 +0200

    Add savers and playlist saver
    Signed-off-by: idan57 <47928001+idan57@users.noreply.github.com>

---
+++ model/savers/playlist_saver.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+
+from model.savers.saver_interface import ISaver
+
+
+class PlaylistSaver(ISaver):
+    """
+    A playlist saver for Song objects.
+    Saves the playlist as a json file.
+    """
+    def __init__(self, name="playlist.json"):
+        """
+        :param name: name for the file that the playlist will be save to
+        """
+        if not name.endswith(".json"):
+            name += ".json"
+        self._name = name
+
+    def save(self, songs):
+        """
+        Get a list of songs and save them to a json file.
+
+        :param songs: list of Songs
+        """
+        playlists_path = Path(__file__).parent.parent.parent / "playlists"
+        playlist_result_path = playlists_path / self._name
+        if not playlists_path.is_dir():
+            playlists_path.mkdir()
+        result_dict = {}
+        for song in songs:
+            result_dict[song.ID] = {
+                "artists": song.Artists,
+                "name": song.Name,
+                "link": song.LinkToSong,
+                "popularity": song.Popularity
+            }
+        result_json = json.dumps(result_dict)
+        playlist_result_path.write_text(result_json)
