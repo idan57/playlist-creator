f9a9ae91b0d3f17bd852610942da12433a7b077b
idan57 <47928001+idan57@users.noreply.github.com>
2020-12-26 18:20:07 +0200

    Fix saver to save json more better
    Signed-off-by: idan57 <47928001+idan57@users.noreply.github.com>

--- model/savers/playlist_saver.py
+++ model/savers/playlist_saver.py
@@ -1,9 +1,9 @@
 import json
 from pathlib import Path
-from threading import Thread
 
 import pylab as plt
-import matplotlib as mpl
+import xlwt
+from matplotlib import artist
 
 from model.savers.saver_interface import ISaver
 
@@ -21,6 +21,7 @@ class PlaylistSaver(ISaver):
         if not name.endswith(".json"):
             name += ".json"
         self._name = name
+        self._playlist_folder = Path(__file__).parent.parent.parent / "playlists"
 
     def save(self, songs):
         """
@@ -28,10 +29,9 @@ class PlaylistSaver(ISaver):
 
         :param songs: list of Songs
         """
-        playlists_path = Path(__file__).parent.parent.parent / "playlists"
-        playlist_result_path = playlists_path / self._name
-        if not playlists_path.is_dir():
-            playlists_path.mkdir()
+        playlist_result_path = self._playlist_folder / self._name
+        if not self._playlist_folder.is_dir():
+            self._playlist_folder.mkdir()
         result_dict = {
             "songs": {}
         }
@@ -43,11 +43,13 @@ class PlaylistSaver(ISaver):
         total_time_in_ms = 0
         n = 0
         for song in songs:
+            link = artist.Artist()
             table += [[n, song.Popularity, song.LinkToSong]]
             y += [song.Popularity]
             n += 1
             x += [n]
-            rows += [f"{song.Artists[0]} {song.Name}"]
+            ar = song.Artists[0].replace("$", "")
+            rows += [f"{ar} - {song.Name}"]
             result_dict["songs"][song.ID] = {
                 "artists": song.Artists,
                 "name": song.Name,
@@ -57,34 +59,48 @@ class PlaylistSaver(ISaver):
             total_time_in_ms += song.Duration
 
         result_dict["total_time_in_ms"] = total_time_in_ms
-        result_json = json.dumps(result_dict)
+        result_json = json.dumps(result_dict, indent=4, sort_keys=True)
         playlist_result_path.write_text(result_json)
 
-        self._show_table_graph(table, rows, cols, x, y)
+        self._show_table_graph(x, y)
+        self._save_to_excel(table, rows, cols)
 
-    def _show_table_graph(self, table, rows, cols, x, y):
+    def _show_table_graph(self, x, y):
         """
         Show the table and graph of the playlist created.
         The graph is the id of the song to its popularity
-        :param table: table contents
-        :param rows: songs as row labels of the table
-        :param cols: cols labels of the table
+
         :param x: ids of the songs
         :param y: popularities
         """
-        f = plt.figure(1)
-        the_table = plt.table(cellText=table,
-                              colWidths=[0.4] * 3,
-                              rowLabels=rows,
-                              colLabels=cols,
-                              loc='center')
-        the_table.auto_set_font_size(False)
-        mng = plt.get_current_fig_manager()
-        mng.resize(*mng.window.maxsize())
-        plt.axis('off')
-        f.show()
-
         g = plt.figure(2)
         plt.plot(x, y)
-        g.show()
-        plt.show()
+        plt.ylabel("Songs Popularity")
+        plt.xlabel("Songs IDs")
+        g.savefig(str(self._playlist_folder / "playlist_graph.pdf"))
+
+    def _save_to_excel(self, table, rows, cols):
+        book = xlwt.Workbook(encoding='utf8')
+        sheet = book.add_sheet("playlist")
+
+        style = xlwt.easyxf('pattern: pattern solid, fore_colour light_blue;'
+                            'font: colour white, bold True;')
+
+        style_c = xlwt.easyxf('pattern: pattern solid, fore_colour light_yellow;'
+                              'font: bold True;')
+        max_widths = [0] * (len(cols) + 1)
+        for i, col in zip(range(len(cols)), cols):
+            max_widths[i + 1] = max(max_widths[i + 1], len(str(col)))
+            sheet.col(i + 1).width = max_widths[i + 1] * 500
+            sheet.write(0, i + 1, col, style)
+
+        for i, row, t in zip(range(1, len(rows) + 2), rows, table):
+            max_widths[0] = max(max_widths[0], len(str(row)))
+            sheet.col(0).width = max_widths[0] * 500
+            sheet.write(i, 0, row, style)
+            for j, t_val in zip(range(1, len(t) + 1), t):
+                max_widths[j - 1] = max(max_widths[j - 1], len(str(t_val)))
+                sheet.col(j).width = max_widths[j - 1] * 500
+                sheet.write(i, j, t_val, style_c)
+
+        book.save(str(self._playlist_folder / "playlist_table.xls"))
